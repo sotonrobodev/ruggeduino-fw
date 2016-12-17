b93cf87d9d6d53489479e531a79fecb09f734542
Kier Davis <kierdavis@gmail.com>
2016-12-17 10:04:31 +0000

    Initial version of servo driver function

--- src/ruggeduino.cpp
+++ src/ruggeduino.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 // NB: If you make any changes to this file, then the sample source for
 // teams to extend in srweb (resources/kit/ruggeduino-fw.ino) will also
@@ -9,8 +12,15 @@
 // sr-robot API layer (in ruggeduino.py)
 #define SERIAL_BAUD 115200
 
+// Determined through experimentation.
+#define SERVO_TICKS_MIN 150
+#define SERVO_TICKS_MAX 600
+
 void setup() {
   Serial.begin(SERIAL_BAUD);
+
+  pwm.begin();
+  pwm.setPWMFreq(60);
 }
 
 int read_pin() {
@@ -48,6 +58,8 @@ void command_mode(int mode) {
 }
 
 void loop() {
+  int channel, pulseTicks;
+
   // Fetch all commands that are in the buffer
   while (Serial.available()) {
     int selected_command = Serial.read();
@@ -75,8 +87,26 @@ void loop() {
         command_mode(INPUT_PULLUP);
         break;
       case 'v':
-        Serial.print("SRduino:");
+        Serial.print("SRDduino:");
         Serial.print(FW_VER);
+        break;
+      case 's':
+        // TODO: improve this protocol.
+        // All other commands use a pure ASCII protocol, but is
+        // this restriction really necessary?
+        while (!Serial.available()) {}
+        channel = Serial.read() - 'a';
+        while (!Serial.available()) {}
+        pulseTicks = Serial.read() << 8;
+        while (!Serial.available()) {}
+        pulseTicks |= Serial.read();
+        if (channel >= 0 && channel <= 15) {
+          pulseTicks = constrain(pulseTicks, SERVO_TICKS_MIN, SERVO_TICKS_MAX);
+          // pulseTicks - the number of 'ticks' that the output should be high for
+          // 1 tick is 1/4096 of a PWM period
+          // PWM frequency is 60 Hz
+          pwm.setPWM(channel, 0, pulseTicks);
+        }
         break;
       default:
         // A problem here: we do not know how to handle the command!
